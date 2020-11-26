f8f65aba6b37847a962c84a9bf2a6ea148e6d576
structsize <52183019+structsize@users.noreply.github.com>
2020-11-26 20:42:04 +0900

    [UPDATE] update set day of week

--- app/src/main/java/com/example/android/SetTime.kt
+++ app/src/main/java/com/example/android/SetTime.kt
@@ -20,8 +20,65 @@ class SetTime : AppCompatActivity() {
             }
         }
 
+        monday.setOnClickListener {
+            if(monday.currentTextColor == Color.BLACK){
+                monday.setTextColor(Color.WHITE)
+                monday.setBackgroundColor(Color.BLACK)
+            } else {
+                monday.setTextColor(Color.BLACK)
+                monday.setBackgroundColor(Color.WHITE)
+            }
+        }
 
+        tuesday.setOnClickListener {
+            if(tuesday.currentTextColor == Color.BLACK){
+                tuesday.setTextColor(Color.WHITE)
+                tuesday.setBackgroundColor(Color.BLACK)
+            } else {
+                tuesday.setTextColor(Color.BLACK)
+                tuesday.setBackgroundColor(Color.WHITE)
+            }
+        }
 
+        wednesday.setOnClickListener {
+            if(wednesday.currentTextColor == Color.BLACK){
+                wednesday.setTextColor(Color.WHITE)
+                wednesday.setBackgroundColor(Color.BLACK)
+            } else {
+                wednesday.setTextColor(Color.BLACK)
+                wednesday.setBackgroundColor(Color.WHITE)
+            }
+        }
+
+        thursday.setOnClickListener {
+            if(thursday.currentTextColor == Color.BLACK){
+                thursday.setTextColor(Color.WHITE)
+                thursday.setBackgroundColor(Color.BLACK)
+            } else {
+                thursday.setTextColor(Color.BLACK)
+                thursday.setBackgroundColor(Color.WHITE)
+            }
+        }
+
+        friday.setOnClickListener {
+            if(friday.currentTextColor == Color.BLACK){
+                friday.setTextColor(Color.WHITE)
+                friday.setBackgroundColor(Color.BLACK)
+            } else {
+                friday.setTextColor(Color.BLACK)
+                friday.setBackgroundColor(Color.WHITE)
+            }
+        }
+
+        saturday.setOnClickListener {
+            if(saturday.currentTextColor == Color.BLUE){
+                saturday.setTextColor(Color.WHITE)
+                saturday.setBackgroundColor(Color.BLUE)
+            } else {
+                saturday.setTextColor(Color.BLUE)
+                saturday.setBackgroundColor(Color.WHITE)
+            }
+        }
 
     }
 }
