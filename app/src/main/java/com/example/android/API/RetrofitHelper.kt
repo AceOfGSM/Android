fb6a8f16a3595a7017ae41069d268a1da9afb94c
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-12-05 01:12:59 +0900

    <Create> Retrofit SignUp

--- app/src/main/java/com/example/android/API/RetrofitHelper.kt
+++ app/src/main/java/com/example/android/API/RetrofitHelper.kt
@@ -6,9 +6,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitHelper {
 
     private var retrofit = Retrofit.Builder()
-        .baseUrl("")
+        .baseUrl("http://192.168.0.112:8000")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    fun getUserAPI() : UserAPI{
+        return retrofit.create(UserAPI::class.java)
+    }
 
+    fun getSleepAPI() : SleepAPI{
+        return retrofit.create(SleepAPI::class.java)
+    }
 }
--- app/src/main/java/com/example/android/SignUpActivity.kt
+++ app/src/main/java/com/example/android/SignUpActivity.kt
@@ -3,7 +3,14 @@ package com.example.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.android.API.RetrofitHelper
+import com.example.android.DTO.Signup
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +19,26 @@ class SignUpActivity : AppCompatActivity() {
 
         signupButton.setOnClickListener {
             if(signupName.text != null){
-                val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
-                intent.putExtra("name", signupName.text.toString())
-                setResult(1,intent)
-                finish()
+                if (signupPwd.text.toString() == signupPwdconfirm.text.toString()){
+                    RetrofitHelper().getUserAPI().signUp(signupID.text.toString(),signupPwd.text.toString(), signupName.text.toString()).enqueue(object : Callback<Signup>{
+                        override fun onResponse(call: Call<Signup>, response: Response<Signup>) {
+                            if(response.isSuccessful){
+                                val intent = Intent(this@SignUpActivity,LoginActivity::class.java)
+
+                                startActivity(intent)
+                            }
+
+                        }
+
+                        override fun onFailure(call: Call<Signup>, t: Throwable) {
+                            Toast.makeText(this@SignUpActivity,"에러",Toast.LENGTH_SHORT).show()
+                            Log.e("ERROR", t.toString())
+                        }
+
+                    })
+                }else{
+                    Toast.makeText(this,"비밀번호가 맞지 않습니다.",Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
