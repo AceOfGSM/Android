f4cc63c004b549c059082e0a5cfece0b0f598620
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-12-05 01:13:17 +0900

    <Create> Retrofit Login

--- app/src/main/java/com/example/android/LoginActivity.kt
+++ app/src/main/java/com/example/android/LoginActivity.kt
@@ -3,22 +3,45 @@ package com.example.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.example.android.API.RetrofitHelper
+import com.example.android.DTO.Responselogin
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
-    var id : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loginButton.setOnClickListener {
+            RetrofitHelper().getUserAPI().login(loginID.text.toString(),loginPwd.text.toString()).enqueue(object : Callback<Responselogin>{
+                override fun onResponse(
+                    call: Call<Responselogin>,
+                    response: Response<Responselogin>
+                ) {
+                    if (response.isSuccessful) {
+                        val intent = Intent(this@LoginActivity, MainUserActivity::class.java)
+                        intent.putExtra("userID",response.body()!!.user.name)
+                        intent.putExtra("token",response.body()!!.token)
+                        startActivity(intent)
+                    }
+                }
 
-    }
+                override fun onFailure(call: Call<Responselogin>, t: Throwable) {
+                    Log.d("ERROR", t.toString())
+                }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(data != null){
-            id = data.getStringExtra("name")!!
+            })
+        }
+        btnGoSignUp.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
+
 }
